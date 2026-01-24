9369610262e100bb49a8ebe9cb90b07393c12491
YousefHassan52 <yousef52hm@gmail.com>
2026-01-24 23:25:53 +0200

    get user by id

--- src/main/java/com/example/ecommerce_spring/controllers/UserController.java
+++ src/main/java/com/example/ecommerce_spring/controllers/UserController.java
@@ -4,14 +4,23 @@ import com.example.ecommerce_spring.entities.User;
 import com.example.ecommerce_spring.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private UserRepository userRepository;
-    @GetMapping("/users")
+    @GetMapping("")
     public Iterable<User> users() {
         return userRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(null);
+    }
+
 }
