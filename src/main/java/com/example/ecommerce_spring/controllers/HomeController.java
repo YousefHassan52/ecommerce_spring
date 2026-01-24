8d66150512c4570c4adaa89b34f7ce2fd20e536f
YousefHassan52 <yousef52hm@gmail.com>
2026-01-24 21:43:14 +0200

    thymleaf dependency

--- src/main/java/com/example/ecommerce_spring/controllers/HomeController.java
+++ src/main/java/com/example/ecommerce_spring/controllers/HomeController.java
@@ -1,17 +1,19 @@
 package com.example.ecommerce_spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    public String index(Model model) {
+        model.addAttribute("name", "Yousef Hassan");
+        return "index";
     }
     @RequestMapping("/name")
     public String name() {
-        return "say_my_name.html";
+        return "say_my_name";
     }
 }
