ad1c2b76fe67a41edc416ef66e6ca33e831651f9
YousefHassan52 <yousef52hm@gmail.com>
2026-03-06 00:17:23 +0200

    jwt service v2

--- src/main/java/com/example/ecommerce_spring/controllers/AuthController.java
+++ src/main/java/com/example/ecommerce_spring/controllers/AuthController.java
@@ -1,9 +1,11 @@
 package com.example.ecommerce_spring.controllers;
 
 import com.example.ecommerce_spring.dtos.ErrorDto;
+import com.example.ecommerce_spring.dtos.JwtResponse;
 import com.example.ecommerce_spring.dtos.UserLoginDto;
 import com.example.ecommerce_spring.exceptions.EmailOrPasswordNotCorrectException;
 import com.example.ecommerce_spring.repositories.UserRepository;
+import com.example.ecommerce_spring.services.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 
 public class AuthController {
+    private final JwtService jwtService;
 
     private final AuthenticationManager authenticationManager;
 
@@ -34,8 +37,9 @@ public class AuthController {
             )
         );
 
-        return ResponseEntity.noContent().build();
+        var token = jwtService.generateToken(userLoginDto.getEmail());
 
+        return ResponseEntity.ok(new JwtResponse(token));
 
     }
 
